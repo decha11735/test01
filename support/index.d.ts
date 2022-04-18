497aa20290eb89785987ada37b5c69463096ea51
decha11735 <103180457+decha11735@users.noreply.github.com>
2022-04-18 13:33:56 +0700

    Delete support directory

--- support/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace Cypress {
-  interface Chainable {
-    login(email: string, password: string): Chainable<Element>;
-    login_successfully(email: string, password: string): Chainable<Element>;
-    typeLogin(user:[]): Chainable<Element>;
-  }
-}
